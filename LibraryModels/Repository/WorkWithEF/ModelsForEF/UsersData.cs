58d527a9042f039a68b53b0aad9440b28ccbdc8b
Денис Макавчик <makavchik1.19@gmail.com>
2023-03-12 20:12:09 +0300

    Error

--- LibraryModels/Repository/WorkWithEF/ModelsForEF/UsersData.cs
+++ LibraryModels/Repository/WorkWithEF/ModelsForEF/UsersData.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization
+using System.Text.Json.Serialization;
 
 namespace LibraryModels.Repository
 {
